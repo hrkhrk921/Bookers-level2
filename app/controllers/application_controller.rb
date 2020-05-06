9fe66a4a3c4e29fab23f2b70ba97f99540f824ba
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-05-06 19:04:12 +0900

    ログイン後画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+    #ログイン済みユーザーのみにアクセスを許可する
+	before_action :authenticate_user!
+
+	#deviseコントローラーにストロングパラメータを追加
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	protected
 	def configure_permitted_parameters
+		#サインアップジ時にnameのストロングパラメータを追加
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		#アカウント編集の時にnameとprofileのストロングパラメータを追加
+		devise_parameter_sanitizer.permit(:account_update, keys:[:name, :introduction])
 	end
 end
