72f58bee5a68a5594556129610cb0b47e74f4fe0
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-05-07 14:59:46 +0900

    usersコントローラー作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,18 @@
 class ApplicationController < ActionController::Base
-    #ログイン済みユーザーのみにアクセスを許可する
-	before_action :authenticate_user!
 
 	#deviseコントローラーにストロングパラメータを追加
 	before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
+
+
+	
 	protected
 	def configure_permitted_parameters
-		#サインアップジ時にnameのストロングパラメータを追加
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-		#アカウント編集の時にnameとprofileのストロングパラメータを追加
-		devise_parameter_sanitizer.permit(:account_update, keys:[:name, :introduction])
+		#サインアップ時にnameのストロングパラメータを追加
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+
 	end
 end
